96e1f32cffa23c112ba76becac084b2764be62be
Iknip26 <ikhwanhaniffirdaus@mail.ugm.ac.id>
2024-06-09 18:11:30 +0700

    ffg

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        first_color: "#6dc5d1",
+        second_color: "#fde49e",
+        third_color: "#feb941",
+        fourth_color: "#4CCD99",
+        fifth_color: "#D04848",
+        sixth_color: "#5F5D9C",
+        seventh_color: "#F6995C",
+        eighth_color: "#D37676",
+        ninth_color: "#EAECCC",
+        text_color_dark: "#0c0c0c",
+        text_color_light: "#e2dfd0",
+      },
+    },
+  },
+  plugins: [],
+};
